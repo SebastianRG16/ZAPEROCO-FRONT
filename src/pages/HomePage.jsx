7649c6d660b897f9478a4aece3dad99ec7d4f9ac
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2023-11-08 17:45:33 -0500

    redireccion

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -140,13 +140,14 @@ export function HomePage() {
                   </Link>
                 </li>
               </ul>
-              <button
+              <Link
+                to="/"
                 className="middle none center mb-2 block w-full rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-ripple-light="true"
               >
                 <span>Salir</span>
-              </button>
+              </Link>
             </div>
           </div>
         </div>
